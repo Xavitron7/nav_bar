8932de28596ebe028e6ae49d2915b8473cfe316b
Sean Brown <seanbrown82@hotmail.com>
2021-03-07 20:34:57 +0000

    no comments2

---
+++ src/displaymenu.js
@@ -0,0 +1,12 @@
+const displayMenu = event => {
+    const newsDrop = document.querySelector("#news-drop");
+
+
+    newsDrop.classList.toggle("hidden")
+
+
+
+}
+
+
+export default displayMenu;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import displayMenu from "./displaymenu";
+
+const newsButton = document.querySelector("#news-head");
+
+newsButton.addEventListener("click", displayMenu)
